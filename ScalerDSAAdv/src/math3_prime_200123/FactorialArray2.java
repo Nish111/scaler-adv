bf19e17334e34865d8f3e681bb9053064f52a99d
Nishant Banginwar <banginwar.nishant1@gmail.com>
2023-03-15 08:47:17 +0530

    check

---
+++ ScalerDSAAdv/src/math3_prime_200123/FactorialArray2.java
@@ -0,0 +1,60 @@
+package math3_prime_200123;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class FactorialArray2 {
+    public int getMax(int[] A){
+        int N=A.length,max = 1<<31;
+        for(int i:A){
+            max = Math.max(max,i);
+        }
+        System.out.println(max);
+        return max;
+    }
+    public void findSPF(int max,int[] spf){
+        Arrays.fill(spf,1);
+        int pre=1,x = (int)Math.sqrt(max);
+        for(int i=2;i<=x;i++){
+            if(spf[i]==1){
+                for(int j=i*i;j<=max;j+=i){
+                    spf[j]=0; // marking non-prime numbers
+                }
+            }
+        }
+        for(int i=2;i<=max;i++){
+            if(spf[i]==1){
+                pre =i; //using carry forward approach to fill the largest prime number
+            }
+            spf[i]=pre;
+        }
+    }
+    public int solve(int[] A) {
+        int max = getMax(A),ans=0; //get max from A
+        int[] spf = new int[max+1];
+        printArray(spf);
+        findSPF(max,spf);        // filling spf array with largest prime numbers till there
+        printArray(spf);
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i:A){
+            if(i==1) continue;
+            hm.put(spf[i],hm.getOrDefault(spf[i],0)+1);
+            ans += 1<<hm.get(spf[i])-1; // for every new occurrence, 2^count -1 subseq’s are possible
+        }
+        return ans;
+    }
+    public void printArray(int[] A) {
+		for(int i=0; i<A.length; i++)
+			System.out.print(A[i]+ " ");
+		System.out.println();
+	}
+    public static void main(String[] args) {
+    	FactorialArray2 fa = new FactorialArray2();
+		int[] A = {2,3,2,3};
+		System.out.println(fa.solve(A));// 6 -  2 2 6 6 2,2 6,6
+		int[] B = {2,3,4}; // 4 - 2 6 24 6,24
+		System.out.println(fa.solve(B));
+		int[] C = {5,10,7,3,4};
+		System.out.println(fa.solve(C));
+	}
+}
