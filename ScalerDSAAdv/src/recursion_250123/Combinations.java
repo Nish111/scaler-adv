a214237bd82d73b6c251757633d5dcd8a5fdbc4e
Nishant Banginwar <banginwar.nishant1@gmail.com>
2023-03-15 08:47:04 +0530

    old

--- ScalerDSAAdv/src/recursion_250123/Combinations.java
+++ ScalerDSAAdv/src/recursion_250123/Combinations.java
@@ -28,14 +28,59 @@ public class Combinations { // failing, need different approach
 		if(b==1 || b==0) return 1;
 		return b*factorial(b-1);
 	}
-	 private ArrayList < ArrayList < Integer >> ans;
-	   public ArrayList < ArrayList < Integer >> combine2(int A, int B) { // not working
-	        ans = new ArrayList < > ();
-	        ArrayList < Integer > cur = new ArrayList < > ();
-	        solve(1, cur, A, B);
+	// working now
+	 private ArrayList<ArrayList<Integer>> ans;
+	   public ArrayList<ArrayList<Integer>> combine3(int A, int B) { // not working
+	        ans = new ArrayList <> ();
+	        ArrayList<Integer> cur = new ArrayList <> ();
+	        solve(ans, cur, A, B, 1);
 	        return ans;
 	    }
-	    void solve(int idx, ArrayList < Integer > cur, int A, int B) {
+	    void solve(ArrayList<ArrayList<Integer>> ans,ArrayList<Integer> cur, int A, int B, int index) {
+	        if (cur.size() == B) {
+	            ans.add(new ArrayList < > (cur));
+	            return;
+	        }
+	        if (index == A + 1) 
+	            return;
+	        // Include current element
+	        cur.add(index);
+	        solve(ans, cur, A, B, index + 1);
+	        cur.remove(cur.size() - 1);
+	        // Don't include current element
+	        solve(ans, cur, A, B, index + 1);
+	    }
+	    //
+	    public static ArrayList<ArrayList<Integer>> combine2(int A, int B) {
+	        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+	        finalCombine2(res, new ArrayList<Integer>(), A, B, 1);
+	        return res;
+	    }
+
+	    private static void
+	    finalCombine2(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> temp, int A, int size, int index) {
+
+	        if (temp.size() == size) {
+	            res.add(new ArrayList<>(temp));
+	            return;
+	        }
+	        for (int i = index; i <=A; i++) {
+	            if (temp.contains(i)) continue;
+	            temp.add(i);
+	            finalCombine2(res, temp, A, size, i + 1);
+	            //  this is shared array for all the stack calls , so we undo the array to previous stack
+	            temp.remove(temp.size() - 1);
+
+	        }
+	    }
+	    private ArrayList < ArrayList < Integer >> ansScalerSol;
+	    public ArrayList < ArrayList < Integer >> combineScalerSol(int A, int B) {
+	        ansScalerSol = new ArrayList < > ();
+	        ArrayList < Integer > cur = new ArrayList < > ();
+	        solveScalerSol(1, cur, A, B);
+	        return ansScalerSol;
+	    }
+	    void solveScalerSol(int idx, ArrayList < Integer > cur, int A, int B) {
 	        if (cur.size() == B) {
 	            ans.add(new ArrayList < > (cur));
 	            return;
@@ -44,10 +89,15 @@ public class Combinations { // failing, need different approach
 	            return;
 	        // Include current element
 	        cur.add(idx);
-	        solve(idx + 1, cur, A, B);
+	        solveScalerSol(idx + 1, cur, A, B);
 	        cur.remove(cur.size() - 1);
 	        // Don't include current element
-	        solve(idx + 1, cur, A, B);
+	        solveScalerSol(idx + 1, cur, A, B);
+	    }
+	    public void printArray(int[][] A) {
+	    	for(int i=0; i<A.length; i++) 
+				System.out.println(A[i][0] + " " + A[i][1]);
+			System.out.println();
 	    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -56,14 +106,9 @@ public class Combinations { // failing, need different approach
 		int[][] B = c.combine(3, 2); // 1 2  1 3  2 3
 		int[][] C = c.combine(1, 1); // 1
 		//int[][] D = c.combine(3, 3); // not working for these, need to make generic
-		for(int i=0; i<A.length; i++) {
-			System.out.println(A[i][0] + " " + A[i][1]);
-		}
-		System.out.println();
-		for(int i=0; i<B.length; i++) {
-			System.out.println(B[i][0] + " " + B[i][1]);
-		}
-		System.out.println();
+		c.printArray(A);
+		c.printArray(B);
+		//c.printArray(C);
 		for(int i=0; i<C.length; i++) {
 			System.out.println(C[i][0] + " " );
 		}
