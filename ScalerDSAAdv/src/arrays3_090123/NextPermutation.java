90e693901b32742ed2e5b2a533865c5a18957b14
Nishant Banginwar <banginwar.nishant1@gmail.com>
2023-03-19 23:04:17 +0530

    math

--- ScalerDSAAdv/src/arrays3_090123/NextPermutation.java
+++ ScalerDSAAdv/src/arrays3_090123/NextPermutation.java
@@ -1,41 +1,45 @@
 package arrays3_090123;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-
+import java.util.Collections;
+// https://www.scaler.com/academy/mentee-dashboard/class/50128/homework/problems/71/?navref=cl_pb_nv_tb
 public class NextPermutation {
 
-	public ArrayList<Integer> nextPermutation(ArrayList<Integer> A) {
-	
-		return A;
-	}
 	//1
-	public int[] nextPermutation(int[] A) {
+	public int[] nextPermutation(int[] A) { // working
 		  if(A.length<=1) return A;
-	        int i = A.length - 2;
-	        while(i >= 0 && A[i] >= A[i+1]) i--;
-	        if(i >= 0){
-	            int j = A.length-1;
-	            while(j >= 0 && A[j] <= A[i]) j--;
-	            swap(A, i, j);
-	        }
-	        reverse(A,i+1);
-	        return A;
+	      int len = A.length ;
+	      for(int i=len-2; i>=0; i--) {
+	    	  if(A[i+1]>A[i]) {
+	    		  for(int j=len-1; j>i; j--) {
+	    			  if(A[j]>A[i]) {
+	    				  int temp = A[i];
+	    				  A[i] = A[j];
+	    				  A[j] = temp;
+	    				  break;
+	    			  }
+	    		  }
+	    		  reverse(A, i+1, len-1);
+	    		  return A;
+	    	  }
+	      }
+	      reverse(A,0,len-1);
+	      return A;
 	    }
 
-	    public void swap(int A[], int i, int j){
-	        int temp = A[i];
-	        A[i] = A[j];
-	        A[j] = temp;
-	    }
-
-	    public void reverse(int[] A, int start){
-	        int end = A.length-1;
+	    public void reverse(int[] A, int start, int end){
 	        while(start<end){
-	            swap(A, start, end);
+	        	int temp = A[start];
+	        	A[start] = A[end];
+	        	A[end] = temp;
 	            start++;
 	            end--;
 	        }
+	    }
+	    public void printArray(int[] A) {
+	    	for(int i=0; i<A.length; i++)
+	    		System.out.print(A[i] +" ");
+	    	System.out.println();
 	    }
 	    //1
 	    //2
@@ -57,7 +61,7 @@ public class NextPermutation {
 	        if(i>=0){
 	            int j = A.length - 1;
 	            while(A[j]<=A[i])j--;
-	            swap(A, i, j);
+	            //swap(A, i, j);
 	        }
 	        reverse2(A,i+1,A.length-1);
 	        return A;
@@ -75,20 +79,76 @@ public class NextPermutation {
 	        }
 	    }
 	    //2
+	    public ArrayList<Integer> nextPermutationScalerSol(ArrayList<Integer> A) {
+		    boolean status;
+		    status = nextPermScalerSol(A);
+		    if (!status)
+		        Collections.sort(A);
+		    return A;
+		}
+		
+		public boolean nextPermScalerSol(ArrayList<Integer> A) {
+		    
+		    int i = 0;
+		    int n = A.size();
+		    
+		    for (i = n - 2; i >= 0; i--) {
+		        if (A.get(i) < A.get(i + 1))
+		            break;
+		    }
+		    
+		    // the array is in descending order
+		    if (i == -1)
+		        return false;
+		   
+		    int j = n - 1;
+		    for (; j >= i; j--) {
+		        if (A.get(j) > A.get(i))
+		            break;
+		    }
+		    
+		    // swap with the smallest number in the suffix
+		    swapScalerSol(A, i, j);
+		    
+		    // reversing the suffix
+		    i++;
+		    int steps = (n - i + 1) / 2;
+		    for (int k = 0; k < steps; k++) {
+		        swapScalerSol(A, i + k, n - k - 1);
+		    }
+		    
+		    return true;
+		}
+		
+		public void swapScalerSol(ArrayList<Integer> A, int i, int j) {
+		    int temp = A.get(i);
+		    A.set(i, A.get(j));
+		    A.set(j, temp);
+		}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		NextPermutation np = new NextPermutation();
+		int[] A = {1,3,2};
+		int[] B = np.nextPermutation(A);
+		np.printArray(B); // 2 1 3 
+		int[] C = {1, 5, 8, 4, 7, 6, 5, 3, 1};
+		int[] D = np.nextPermutation(C);
+		np.printArray(D); // 1 5 8 5 1 3 4 6 7 
+		int[] E = { 5, 4, 3, 1};
+		int[] F = np.nextPermutation(E); // 1 3 4 5 
+		np.printArray(F); // 1 5 8 5 1 3 4 6 7
 		ArrayList<Integer> al = new ArrayList<>();
-		al.add(1,1);
-		al.add(2,2);
-		al.add(3,3);
-		ArrayList<Integer> alr = np.nextPermutation(al);
+		al.add(1);
+		al.add(2);
+		al.add(3);
+		ArrayList<Integer> alr = np.nextPermutationScalerSol(al);
 		ArrayList<Integer> al2 = new ArrayList<>();
-		al2.add(1,3);
-		al2.add(2,2);
-		al2.add(3,1);
-		ArrayList<Integer> al2r = np.nextPermutation(al2);
+		al2.add(1);
+		al2.add(1);
+		al2.add(5);
+		ArrayList<Integer> al2r = np.nextPermutationScalerSol(al2);
 		
 	}
+
 
 }
